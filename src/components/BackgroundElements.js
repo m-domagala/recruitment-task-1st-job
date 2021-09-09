f85172c6a31eb2153096e9ab187055d51f61c02f
Michal Domagala <michaldomagala@hotmail.com>
2021-09-09 20:50:38 +0200

    add b=Board, BackgroundElements and HiddenElements components

---
+++ src/components/BackgroundElements.js
@@ -0,0 +1,11 @@
+export default function BackgroundElements() {
+ return (
+  <div className="background-elements">
+   <div className="mask-copy-3"></div>
+   <div className="path-5"></div>
+   <div className="oval-1"></div>
+   <div className="fill-1-copy-1"></div>
+   <div className="line-6"></div>
+  </div>
+ );
+}
