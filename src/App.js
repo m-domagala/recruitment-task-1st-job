14f819384d858769cc9d9f0652a48c349d73d8c5
Michal Domagala <michaldomagala@hotmail.com>
2021-09-09 20:50:38 +0200

    add Board, BackgroundElements and HiddenElements components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import BackgroundElements from './components/BackgroundElements';
+import Board from './components/Board';
+import HiddenElements from './components/HiddenElements';
+
 export default function App() {
  return (
   <>
-   <div className="wrapper">
-    <main className="board"></main>
+   <div className="app">
+    <Board />
+    <BackgroundElements />
+    <HiddenElements />
    </div>
   </>
  );
