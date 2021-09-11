5a1cf882ac14fd7589182c41971061c5ebff4add
Michal Domagala <michaldomagala@hotmail.com>
2021-09-11 19:42:03 +0200

    add variables and reset scss, add background element

--- src/components/BackgroundElements.js
+++ src/components/BackgroundElements.js
@@ -1,12 +1,25 @@
 export default function BackgroundElements() {
  return (
   <div className="background-elements">
-   <div className="fill-1-copy-1"></div>
+   <div className="fill-1-copy-1">
+    <div className="fill-1-copy-1__container">
+     <div className="fill-1-copy-1__element-1"></div>
+     <div className="fill-1-copy-1__element-2"></div>
+     <div className="fill-1-copy-1__element-3"></div>
+    </div>
+   </div>
+
    <div className="line-6">
     <div className="line-6__line"></div>
    </div>
+
    <div className="mask-copy-3"></div>
-   <div className="oval-1"></div>
+
+   <div className="oval-1">
+    <div className="oval-1__spinner"></div>
+    <div className="oval-1__blank-field"></div>
+   </div>
+
    <div className="path-5"></div>
   </div>
  );
