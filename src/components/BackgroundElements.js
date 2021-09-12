d021e122dabdd0f92615183b77b33bcf037c4901
Michal Domagala <michaldomagala@hotmail.com>
2021-09-12 19:14:51 +0200

    add background snake element

--- src/components/BackgroundElements.js
+++ src/components/BackgroundElements.js
@@ -1,26 +1,34 @@
 export default function BackgroundElements() {
  return (
   <div className="background-elements">
-   <div className="fill-1-copy-1">
-    <div className="fill-1-copy-1__container">
-     <div className="fill-1-copy-1__element-1"></div>
-     <div className="fill-1-copy-1__element-2"></div>
-     <div className="fill-1-copy-1__element-3"></div>
+   <div className="line">
+    <div className="line__line-element"></div>
+   </div>
+
+   <div className="mask-left"></div>
+
+   <div className="mask-right"></div>
+
+   <div className="snake">
+    <div className="snake__container">
+     <div className="snake__element-1">
+      <div className="snake__blank-1"></div>
+     </div>
+     <div className="snake__element-2">
+      <div className="snake__blank-2">
+       <div className="snake__shadow"></div>
+      </div>
+     </div>
+     <div className="snake__element-3">
+      <div className="snake__blank-3"></div>
+     </div>
     </div>
    </div>
 
-   <div className="line-6">
-    <div className="line-6__line"></div>
+   <div className="spinner">
+    <div className="spinner__spinner-element"></div>
+    <div className="spinner__blank"></div>
    </div>
-
-   <div className="mask-copy-3"></div>
-
-   <div className="oval-1">
-    <div className="oval-1__spinner"></div>
-    <div className="oval-1__blank-field"></div>
-   </div>
-
-   <div className="path-5"></div>
   </div>
  );
 }
